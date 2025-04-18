aa75d2de6a508fe2a52f82875eee230197f0e2ec
mzffreyvazov <mzffreyvazov2005@gmail.com>
2025-04-19 01:16:45 +0400

    spring security implementation with jdbc

---
+++ employeeProject-spring-security-database/src/main/java/com/example/employeeproject/security/SecurityConfig.java
@@ -0,0 +1,67 @@
+package com.example.employeeproject.security;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    // add support for jdbc authentication
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    // Restrinc access to the endpoints
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(requests -> requests
+                .requestMatchers(HttpMethod.GET, "/employee/**").hasRole("EMPLOYEE")
+                .requestMatchers(HttpMethod.GET, "/employee").hasRole("EMPLOYEE")
+
+                .requestMatchers(HttpMethod.POST, "/employee").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.PUT, "/employee/**").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.PATCH, "/employee/**").hasRole("MANAGER")
+
+                .requestMatchers(HttpMethod.DELETE, "/employee/**").hasRole("ADMIN")
+                .anyRequest().authenticated())
+            .httpBasic(Customizer.withDefaults()) 
+            .csrf(AbstractHttpConfigurer::disable); 
+
+        return http.build();
+    }
+
+    /* 
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+
+        UserDetails john = User.withUsername("john")
+                .password("{noop}password123") 
+                .roles("EMPLOYEE")
+                .build();
+
+        UserDetails marry = User.withUsername("marry")
+                .password("{noop}password123") 
+                .roles("EMPLOYEE", "MANAGER")
+                .build();
+
+        UserDetails Katy = User.withUsername("katy")
+                .password("{noop}password123") 
+                .roles("EMPLOYEE", "ADMIN", "MANAGER")
+                .build();
+
+        
+        return new InMemoryUserDetailsManager(john, marry, Katy);
+    }
+*/
+}
+
